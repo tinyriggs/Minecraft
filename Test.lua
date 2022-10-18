e1834a3bea264806e02e96813a38ff613de3f150
tinyriggs <nr107984@gmail.com>
2022-10-18 14:24:45 -0400

    more testing

--- Test.lua
+++ Test.lua
@@ -1,6 +1,40 @@
 
 
-while(turtle.getItemCount() > 0)
-do
-    turtle.dropDown()
+X = 17
+Y = 17
+
+
+
+function placeLine()
+    while Y > 0 do
+        turtle.placeDown()
+        turtle.forward()
+        Y = Y - 1
+    end
+    turtle.placeDown()
+    Y = 17
+end
+
+
+function turn()
+    if (X % 2 == 0)then
+        turtle.turnRight()
+    else
+        turtle.turnLeft()
+    end
+
+    turtle.forward()
+
+    if (X % 2 == 0)then
+        turtle.turnRight()
+    else
+        turtle.turnLeft()
+    end
+
+    X = X - 1
+end
+
+while X > 0 do
+    placeLine()
+    turn()
 end
