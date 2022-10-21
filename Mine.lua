2aefb3a44a043ae3d77afa4258699bcd9337819d
tinyriggs <nr107984@gmail.com>
2022-10-21 16:24:55 -0400

    updated mine

--- Mine.lua
+++ Mine.lua
@@ -1,19 +1,32 @@
-local depth = 64
 
-while depth > 0 do
-    turtle.digDown()
-    turtle.down()
-    turtle.dig()
-    turtle.turnLeft()
-    turtle.dig()
-    turtle.turnLeft()
-    turtle.dig()
-    turtle.turnLeft()
-    turtle.dig()
-    turtle.turnLeft()
-    depth = depth - 1
+function mineDown(depth)
+    while depth > 0 do
+        turtle.digDown()
+        turtle.down()
+        turtle.dig()
+        turtle.turnLeft()
+        turtle.dig()
+        turtle.turnLeft()
+        turtle.dig()
+        turtle.turnLeft()
+        turtle.dig()
+        turtle.turnLeft()
+        depth = depth - 1
+    end
 end
 
-for i = 1, 64, 1 do
-    turtle.up()
+function mineForward(distance)
+    while distance > 0 do
+        turtle.dig()
+        turtle.forward()
+        turtle.digUp()
+        turtle.digDown()
+    end
 end
+
+
+mineDown(5)
+
+mineForward(25)
+
+
